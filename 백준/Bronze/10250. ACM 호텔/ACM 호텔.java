788a495528b28a657dc1ca8f3d8ff6c10c816c9f
dodotlm <73534612+dodotlm@users.noreply.github.com>
2024-06-13 09:29:51 +0900

    [Bronze III] Title: ACM 호텔, Time: 236 ms, Memory: 18112 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
- 
-		int T = in.nextInt();	// 테스트 케이스
- 
- 
-		for(int i = 0; i < T; i++) {
-			
-			int H = in.nextInt();
-			int W = in.nextInt(); 	// 쓸모없는 변수다.
-			int N = in.nextInt();
-			
-			if(N % H == 0) {
-				System.out.println((H * 100) + (N / H));
- 
-			} else {
-				System.out.println(((N % H) * 100) + ((N / H) + 1));
-			}
-		}
-	}
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int cnt = sc.nextInt();
+        sc.nextLine();
+        int XX = 0;
+        int YY = 0;
+        int[] arr = new int[cnt];
+        
+        for(int i=0; i<cnt; i++) {
+        	
+        	String temp = sc.nextLine();
+        	String[] tempArr = temp.split(" ");
+        	int Y = Integer.parseInt(tempArr[0]);
+        	int X = Integer.parseInt(tempArr[1]);
+        	int cus = Integer.parseInt(tempArr[2]);
+        	// 받아주기
+        	
+        	if(cus%Y == 0) {
+        		XX = Y*100;
+        		YY = (cus/Y);
+        	}else {
+        		XX = (cus%Y)*100;
+        		YY = ((cus/Y)+1);
+        	}
+        	arr[i] = XX+YY;
+        }
+        
+        for(int j=0; j<cnt; j++) {
+        	System.out.println(arr[j]);
+        }
+    }
 }
