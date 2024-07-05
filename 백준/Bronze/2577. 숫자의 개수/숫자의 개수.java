2ed97e9a723115360ca90200fdea757ae4c565c5
dodotlm <73534612+dodotlm@users.noreply.github.com>
2024-07-05 10:31:17 +0900

    [Bronze II] Title: 숫자의 개수, Time: 184 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,23 +1,31 @@
+
 import java.util.Scanner;
- 
+
 public class Main {
-	public static void main(String[] args) {
- 
-		Scanner sc = new Scanner(System.in);
- 
-		int value = (sc.nextInt() * sc.nextInt() * sc.nextInt());
-		String str = Integer.toString(value);
-		
-		
-		for (int i = 0; i < 10; i++) {
-			int cnt = 0;
-			for (int j = 0; j < str.length(); j++) {
-				if ((str.charAt(j) - '0') == i) {
-					cnt++;
-				}
-			}
-			System.out.println(cnt);
-		}
-		sc.close();
-	}
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        
+        int result = num1 * num2 * num3;
+        
+        String resultStr = Integer.toString(result);
+        
+        int[] cnt = new int[10];
+        
+        for (int i = 0; i < resultStr.length(); i++) {
+            char numChar = resultStr.charAt(i);
+            ++cnt[numChar - '0'];
+        }
+        
+        for (int k = 0; k < 10; k++) {
+            System.out.println(cnt[k]);
+        }
+        
+        sc.close();
+    }
 }
